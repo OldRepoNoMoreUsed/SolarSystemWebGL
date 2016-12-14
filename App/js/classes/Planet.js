78ef24babc14f0a734e7fb07870042f4e1169414
SommerNicolas <nicolas.sommer@he-arc.ch>
2016-12-14 12:07:44 +0100

    Ajout multi cube

---
+++ App/js/classes/Planet.js
@@ -0,0 +1,149 @@
+class Planet{
+  constructor(name, radius, x, y, z, color){
+    this.name = name;
+    this.radius = radius;
+    this.color = color;
+
+    this.vertexBuffer = null;
+    this.indexBuffer = null;
+    this.colorBuffer = null;
+
+    this.x = x;
+    this.y = y;
+    this.z = z;
+
+    this.mvMatrix = mat4.create();
+
+
+    this.init();
+  }
+
+  init(){
+    this.clearBuffers();
+    this.indices = [];
+    this.vertices = [];
+    this.colors = [];
+
+    //Init cube
+    //vertices
+    this.initvertices();
+    this.initcolors();
+    this.initindices();
+
+    //We create the buffers on the GPU
+		this.vertexBuffer = getVertexBufferWithVertices(this.vertices);
+		this.colorBuffer = getVertexBufferWithVertices(this.colors);
+		this.indexBuffer = getIndexBufferWithIndices(this.indices);
+  }
+
+  initvertices(){
+
+    this.vertices.push(-this.radius, -this.radius, this.radius);
+    this.vertices.push(this.radius, -this.radius, this.radius);
+    this.vertices.push(this.radius, this.radius, this.radius);
+    this.vertices.push(-this.radius, this.radius, this.radius);
+
+    this.vertices.push(-this.radius, -this.radius, -this.radius);
+    this.vertices.push(-this.radius, this.radius, -this.radius);
+    this.vertices.push(this.radius, this.radius, -this.radius);
+    this.vertices.push(this.radius, -this.radius, -this.radius);
+
+    this.vertices.push(-this.radius, this.radius, -this.radius);
+    this.vertices.push(-this.radius, this.radius, this.radius);
+    this.vertices.push(this.radius, this.radius, this.radius);
+    this.vertices.push(this.radius, this.radius, -this.radius);
+
+    this.vertices.push(-this.radius, -this.radius, -this.radius);
+    this.vertices.push(this.radius, -this.radius, -this.radius);
+    this.vertices.push(this.radius, -this.radius, this.radius);
+    this.vertices.push(-this.radius, -this.radius, this.radius);
+
+    this.vertices.push(this.radius, -this.radius, -this.radius);
+    this.vertices.push(this.radius, this.radius, -this.radius);
+    this.vertices.push(this.radius, this.radius, this.radius);
+    this.vertices.push(this.radius, -this.radius, this.radius);
+
+    this.vertices.push(-this.radius, -this.radius, -this.radius);
+    this.vertices.push(-this.radius, -this.radius, this.radius);
+    this.vertices.push(-this.radius, this.radius, this.radius);
+    this.vertices.push(-this.radius, this.radius, -this.radius);
+  }
+
+  initcolors(){
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+    this.colors.push(this.color.r, this.color.g, this.color.b,1.0);
+
+  }
+
+  initindices(){
+    //this.indices.push(0, 1, 2);
+    this.indices = [
+    0,  1,  2,      0,  2,  3,    // front
+    4,  5,  6,      4,  6,  7,    // back
+    8,  9,  10,     8,  10, 11,   // top
+    12, 13, 14,     12, 14, 15,   // bottom
+    16, 17, 18,     16, 18, 19,   // right
+    20, 21, 22,     20, 22, 23];    // left
+
+  }
+
+  clearBuffers(){
+    if(this.vertexBuffer !== null)
+		{
+			glContext.deleteBuffer(this.vertexBuffer);
+		}
+		if(this.colorBuffer !== null)
+		{
+			glContext.deleteBuffer(this.colorBuffer);
+		}
+		if(this.indexBuffer !== null)
+		{
+			glContext.deleteBuffer(this.indexBuffer);
+		}
+  }
+
+  draw(){
+    mat4.identity(this.mvMatrix);
+    mat4.translate(this.mvMatrix, this.mvMatrix, vec3.fromValues(this.x, this.y, this.z));
+    //glContext.uniformMatrix4fv(prg.mvMatrixUniform, false, mvMatrix);
+
+    //Transfert des vertices de la planete
+    glContext.bindBuffer(glContext.ARRAY_BUFFER, this.vertexBuffer);
+    glContext.vertexAttribPointer(prg.vertexPositionAttribute, 3, glContext.FLOAT, false, 0, 0);
+    //Transfert couleur de la planete
+    glContext.bindBuffer(glContext.ARRAY_BUFFER, this.colorBuffer);
+    glContext.vertexAttribPointer(prg.colorAttribute, 4, glContext.FLOAT, false, 0, 0);
+    //Transfert index de la planete
+    glContext.bindBuffer(glContext.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+    //Dessinons le cube en LINES
+    glContext.drawElements(glContext.TRIANGLE_STRIP, this.indices.length, glContext.UNSIGNED_SHORT, 0);
+  }
+}
--- App/js/classes/Scene.js
+++ App/js/classes/Scene.js
@@ -1,5 +1,10 @@
+var blue = {r:0.0, g:0.0, b:1.0};
+var red = {r:1.0, g:0.0, b:0.0};
+var green = {r:0.0, g:1.0, b:0.0};
+
 var mvMatrix = mat4.create();
 var pMatrix = mat4.create();
+var translationVec = vec3.create();
 
 var sceneObjects = [];
 var orbits = [];
@@ -7,63 +12,65 @@ var orbits = [];
 function initWebGL(){
   glContext = getGLContext('webgl-canvas');
   initProgram();
+  initCamera();
   initScene();
+}
+
+function initCamera(){
+  mat4.perspective(pMatrix, degToRad(60.0), c_width/c_height, 0.1, 1000.0);
+  glContext.uniformMatrix4fv(prg.pMatrixUniform, false, pMatrix);
 }
 
 function  initScene(){
   //Declaration des planetes
-  var earth = new Planet("Earth", 0.4, {r:0.14, g:0.29, b:0.65}, 0.0, 0.0, 0.0);
-  //var moon = new Planet("Moon", 0.1, {r:1.0, g:0.1, b:0.8}, 0.0, 0.0, 0.0);
+  var soleil = new Planet("Sun", 0.3, 0.0, 0.0, -5.0, red);
+  var mercury = new Planet("Mercury", 0.1, 0.3, 0.0, -5.0, blue);
+  var venus = new Planet("Venus", 0.1, 2.0, 0.0, -5.0, blue);
+  var truc = new Planet("as", 0.05, 0.9, 0.0, -5.0, red);
+
   //peuplage de la Scene
-  sceneObjects.push(earth);
-  //sceneObjects.push(moon);
+  sceneObjects.push(soleil);
+  sceneObjects.push(mercury);
+  sceneObjects.push(venus);
+  sceneObjects.push(truc);
 
-  //var moonEarthOrbit = new Orbit(earth, moon, 1.5, 0.995);
+  var soleilMercuryOrbit = new Orbit(soleil, mercury, 1.5, 0.995);
+  var soleilVenusOrbit = new Orbit(soleil, venus, 3.0, 0.995);
+  var venusTruc = new Orbit(venus, truc, 0.5, 0.995);
+  orbits.push(soleilMercuryOrbit);
+  orbits.push(soleilVenusOrbit);
+  orbits.push(venusTruc);
 
-  //orbits.push(moonEarthOrbit);
-
-  glContext.clearColor(0.9, 0.9, 0.9, 1.0);
   glContext.enable(glContext.DEPTH_TEST);
+  glContext.clearColor(0.0, 0.0, 0.0, 1.0);
+
 
   mat4.identity(pMatrix);
-
   glContext.viewport(0, 0, c_width, c_height);
-  changeProjection();
+
+  initCamera();
+
   renderLoop();
 }
 
 function drawScene(){
+
   glContext.clear(glContext.COLOR_BUFFER_BIT | glContext.DEPTH_BUFFER_BIT);
+
+  //glContext.uniformMatrix4fv(prg.mvMatrixUniform, false, mvMatrix);
 
   for(var i = 0; i < orbits.length; i++){
     orbits[i].tick();
   }
-  mat4.identity(mvMatrix);
-
-  rotateModelViewMatrixUsingQuaternion();
 
   for(i = 0; i < sceneObjects.length; i++){
-    sceneObjects[i].draw(mvMatrix);
+    mat4.identity(mvMatrix);
+    mat4.multiply(mvMatrix, sceneObjects[i].mvMatrix, mvMatrix );
+    rotateModelViewMatrixUsingQuaternion();
+    glContext.uniformMatrix4fv(prg.mvMatrixUniform, false, mvMatrix);
+    sceneObjects[i].draw();
+
   }
-}
-
-var projection = 0;
-function changeProjection(){
-	if(projection)
-	{
-		//setting the projection in perspective
-		mat4.perspective(pMatrix, degToRad(40), c_width / c_height, 0.1, 1000.0);
-		projection = 0;
-	}
-	else
-	{
-		//setting the projection in orthogonal
-		mat4.ortho(pMatrix, -1.2, 1.2, -1.2, 1.2, 1, 10);
-		projection = 1;
-	}
-
-	//Sending the new projection matrix to the shaders
-	glContext.uniformMatrix4fv(prg.pMatrixUniform, false, pMatrix);
 }
 
 function initShaderParameters(prg){
