797a52312a0d7cf303d5e251bfde377bf674b774
SommerNicolas <nicolas.sommer@he-arc.ch>
2017-01-03 14:05:52 +0100

    reparation cam

--- App/js/classes/Scene.js
+++ App/js/classes/Scene.js
@@ -28,48 +28,48 @@ function initCamera(){
 
 function  initScene(){
   //declaration du systeme
-  var SolarSystem = new System("SystemeSolaire");
-  sceneObjects.push(SolarSystem);
+  // var SolarSystem = new System("SystemeSolaire");
+  // sceneObjects.push(SolarSystem);
   //Declaration des planetes
-  // var soleil = new Planet("Sun", 100, 0.0, 0.0, -1000, red);
-  // var mercury = new Planet("Mercury", 0.35, 0.0, 0.0, -10.0, blue);
-  // var venus = new Planet("Venus", 0.87, 0.0, 0.0, -15.0, blue);
-  // var earth = new Planet("Terre", 0.91, 0.0, 0.0, -20.0, blue);
-  // var mars = new Planet("Mars", 0.48, 0.0, 0.0, -25.0, blue);
-  // var jupiter = new Planet("Jupiter", 10, 0.0, 0.0, -30.0, blue);
-  // var saturn = new Planet("Saturne", 8.33, 0.0, 0.0, -35.0, blue);
-  // var uranus = new Planet("Uranus", 3.53, 0.0, 0.0, -40.0, blue);
-  // var neptune = new Planet("Neptune", 3.63, 0.0, 0.0, -10, green);
-  //
+  var soleil = new Planet("Sun", 100, 0.0, 0.0, -1000, red);
+  var mercury = new Planet("Mercury", 0.35, 0.0, 0.0, -10.0, blue);
+  var venus = new Planet("Venus", 0.87, 0.0, 0.0, -15.0, blue);
+  var earth = new Planet("Terre", 0.91, 0.0, 0.0, -20.0, blue);
+  var mars = new Planet("Mars", 0.48, 0.0, 0.0, -25.0, blue);
+  var jupiter = new Planet("Jupiter", 10, 0.0, 0.0, -30.0, blue);
+  var saturn = new Planet("Saturne", 8.33, 0.0, 0.0, -35.0, blue);
+  var uranus = new Planet("Uranus", 3.53, 0.0, 0.0, -40.0, blue);
+  var neptune = new Planet("Neptune", 3.63, 0.0, 0.0, -10, green);
+
   // //peuplage de la Scene
-  // sceneObjects.push(soleil);
-  // sceneObjects.push(mercury);
-  // sceneObjects.push(venus);
-  // sceneObjects.push(earth);
-  // sceneObjects.push(mars);
-  // sceneObjects.push(jupiter);
-  // sceneObjects.push(saturn);
-  // sceneObjects.push(uranus);
-  // sceneObjects.push(neptune);
+  sceneObjects.push(soleil);
+  sceneObjects.push(mercury);
+  sceneObjects.push(venus);
+  sceneObjects.push(earth);
+  sceneObjects.push(mars);
+  sceneObjects.push(jupiter);
+  sceneObjects.push(saturn);
+  sceneObjects.push(uranus);
+  sceneObjects.push(neptune);
 
 
-  // var soleilMercuryOrbit = new Orbit(soleil, mercury, 10, 0.99);
-  // var soleilVenusOrbit = new Orbit(soleil, venus, 15.0, 0.995);
-  // var soleilEarthOrbit = new Orbit(soleil, earth, 20.0, 0.992);
-  // var soleilMarsOrbit = new Orbit(soleil, mars, 25.0, 0.988);
-  // var soleilJupiterOrbit = new Orbit(soleil, jupiter, 30.0, 0.985);
-  // var soleilSaturnOrbit = new Orbit(soleil, saturn, 35.0, 0.982);
-  // var soleilUranusOrbit = new Orbit(soleil, uranus, 40.0, 0.980);
-  // var soleilNeptuneOrbit = new Orbit(soleil, neptune, 45.0, 0.978);
-  //
-  // orbits.push(soleilMercuryOrbit);
-  // orbits.push(soleilVenusOrbit);
-  // orbits.push(soleilEarthOrbit);
-  // orbits.push(soleilMarsOrbit);
-  // orbits.push(soleilJupiterOrbit);
-  // orbits.push(soleilSaturnOrbit);
-  // orbits.push(soleilUranusOrbit);
-  // orbits.push(soleilNeptuneOrbit);
+  var soleilMercuryOrbit = new Orbit(soleil, mercury, 10, 0.99);
+  var soleilVenusOrbit = new Orbit(soleil, venus, 15.0, 0.995);
+  var soleilEarthOrbit = new Orbit(soleil, earth, 20.0, 0.992);
+  var soleilMarsOrbit = new Orbit(soleil, mars, 25.0, 0.988);
+  var soleilJupiterOrbit = new Orbit(soleil, jupiter, 30.0, 0.985);
+  var soleilSaturnOrbit = new Orbit(soleil, saturn, 35.0, 0.982);
+  var soleilUranusOrbit = new Orbit(soleil, uranus, 40.0, 0.980);
+  var soleilNeptuneOrbit = new Orbit(soleil, neptune, 45.0, 0.978);
+
+  orbits.push(soleilMercuryOrbit);
+  orbits.push(soleilVenusOrbit);
+  orbits.push(soleilEarthOrbit);
+  orbits.push(soleilMarsOrbit);
+  orbits.push(soleilJupiterOrbit);
+  orbits.push(soleilSaturnOrbit);
+  orbits.push(soleilUranusOrbit);
+  orbits.push(soleilNeptuneOrbit);
 
   glContext.enable(glContext.DEPTH_TEST);
   glContext.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -89,15 +89,15 @@ function drawScene(){
   //glContext.uniformMatrix4fv(prg.mvMatrixUniform, false, mvMatrix);
 
 
-  // for(var i = 0; i < orbits.length; i++){
-  //   orbits[i].tick();
-  // }
+  for(var i = 0; i < orbits.length; i++){
+    orbits[i].tick();
+  }
 
   for(i = 0; i < sceneObjects.length; i++){
     mat4.identity(mvMatrix);
     mat4.translate(mvMatrix, mvMatrix, vec3.fromValues(translateX, translateY, translateZ));
     mat4.multiply(mvMatrix, sceneObjects[i].mvMatrix, mvMatrix );
-    rotateModelViewMatrixUsingQuaternion(true);
+    rotateModelViewMatrixUsingQuaternion();
     glContext.uniformMatrix4fv(prg.mvMatrixUniform, false, mvMatrix);
     sceneObjects[i].draw();
   }
